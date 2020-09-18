f3f5631dc4c87041a2d3c00b93eb8d1531960ddb
Starlight Romero <starlightromero@gmail.com>
2020-09-17 17:42:55 -0700

    Added name and variety plant validation

--- static/create.js
+++ static/create.js
@@ -1,6 +1,8 @@
 const seed = document.querySelector('select[name="seed"]')
 const plantName = document.querySelector('input[name="plant_name"]')
 const variety = document.querySelector('input[name="variety"]')
+const seedLabel = document.querySelector('.validate')
+const submitBtn = document.querySelector('input[type=submit]')
 
 seed.addEventListener('change', () => {
   const text = seed.options[seed.selectedIndex].text
@@ -9,3 +11,14 @@ seed.addEventListener('change', () => {
   console.log(plantName)
   variety.value = text.match(/\((.*)\)/).pop()
 })
+
+const validate = () => {
+  const plantNameValue = plantName.value
+  const varietyValue = variety.value
+  if (!plantNameValue || !varietyValue) {
+    seedLabel.style.display = 'block'
+    return false
+  }
+}
+
+submitBtn.onclick = validate
